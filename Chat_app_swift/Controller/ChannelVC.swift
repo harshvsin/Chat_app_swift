b36c1dec87878be7eb531033ffdccb68c3c60dd1
Harsh Vardhan Singh <harshvsin@gmail.com>
2018-09-02 20:46:04 +0530

    Create Account UI Complete

--- Chat_app_swift/Controller/ChannelVC.swift
+++ Chat_app_swift/Controller/ChannelVC.swift
@@ -12,6 +12,8 @@ class ChannelVC: UIViewController {
 
     @IBOutlet weak var loginBtn: UIButton!
     //we created this outlet cuz it need to be changed later
+    @IBAction func prepForUnwind(segue: UIStoryboardSegue){}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
